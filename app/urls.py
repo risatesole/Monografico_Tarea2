55b0561b45b805cfca7266c7f2416f03cb041be1
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-03-22 22:35:47 -0400

    feat: transition views to use HTML templates
    - Configure 'templates' directory in settings.py
    - Refactor homePage and applicationPage to use render() instead of HttpResponse
    - Add base.html for template inheritance
    - Create home.html and app.html templates

--- app/urls.py
+++ app/urls.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
-
+from django.shortcuts import render  # Import render instead of HttpResponse
 
 def homePage(request):
-    return HttpResponse('<h1>Welcome to Home page</h1><a href="/app/">Go to application</a>')
+    # This looks into templates/pages/home.html
+    return render(request, 'pages/home.html')
 
 def applicationPage(request):
-    return HttpResponse("main application page")
+    # If you have an app-specific page, render it here too
+    return render(request, 'pages/app.html') 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homePage),
-    path('app/' ,applicationPage)
+    path('app/', applicationPage)
 ]
