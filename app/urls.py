d0e6c7a8c4d2818a541cd6d1dc5d91a63136ffae
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-03-23 09:14:57 -0400

    Add app page template and styles
    - Create `app.css` with responsive layout for header, hero, and footer sections
    - add app.html with css style

--- app/urls.py
+++ app/urls.py
@@ -8,10 +8,18 @@ def homePage(request):
 
 def applicationPage(request):
     # If you have an app-specific page, render it here too
-    return render(request, 'pages/app.html') 
+    return render(request, 'pages/app.html')
+
+def signUpPage(request):
+    return render(request, 'pages/signup.html')
+
+def signInPage(request):
+    return render(request, 'pages/signin.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homePage),
-    path('app/', applicationPage)
+    path('app/', applicationPage),
+    path('signup/',signUpPage),
+    path('signin/',signInPage)
 ]
