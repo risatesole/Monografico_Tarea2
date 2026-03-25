a14c14d0c9853f2ba0f60d2308250ff0b95056a9
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-03-25 11:27:42 -0400

    feat: signup now storges user information in database

--- applications/account/views.py
+++ applications/account/views.py
@@ -1,13 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 def signup(request):
-    context = {
-        "title": "AvantKeel"
-    }
-    return render(request, 'signup.html', context)
+    if request.method == "POST":
+        full_name = request.POST.get("full_name")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        if User.objects.filter(username=email).exists():
+            messages.error(request, "User with this email already exists.")
+        else:
+            user = User.objects.create_user(
+                username=email,
+                email=email,
+                password=password,
+                first_name=full_name.split(" ")[0],
+                last_name=" ".join(full_name.split(" ")[1:]),
+            )
+            messages.success(request, "Account created successfully!")
+            return redirect("/signin/")
+
+    return render(request, 'signup.html', {"title": "AvantKeel"})
+
 
 def signin(request):
-    context = {
-        "title": "AvantKeel"
-    }
-    return render(request, 'signin.html', context)
+    return render(request, 'signin.html', {"title": "AvantKeel"})
