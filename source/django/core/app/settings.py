db7d4f709ae0e8bd37c468d8a258b689625ebf45
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-04-07 10:32:21 -0400

    feat: define LOGIN_URL for user authentication

--- source/django/core/app/settings.py
+++ source/django/core/app/settings.py
@@ -18,6 +18,7 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+LOGIN_URL = "signin"
 
 
 # Quick-start development settings - unsuitable for production
